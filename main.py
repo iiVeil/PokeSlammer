6337c7aabc26d3307da44100b6294a71137772a7
iiVeil <52388052+iiVeil@users.noreply.github.com>
2019-08-16 20:00:51 -0400

    Added console command "uptime" | Fixed small print bugs

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ async def console_commands_():
                 client.spammer = not client.spammer
                 print(f'{timef} [SPAMMER] -> TOGGLED {client.spammer}\n')
             else:
-                print(f'{timef} [CONSOLE] -> Active guild has not been set this session run `[]set_guild` and `[]set_channel` in a channel or set them both with `[]set`.\n')
+                print(f'{timef} [CONSOLE] -> Active guild has not been set this session run `[]set` in a channel.\n')
         elif str.lower(input) == 'uptime':
             current_time = time.time()
             difference = int(round(current_time - start_time))
@@ -236,7 +236,7 @@ async def console_commands_():
             await client.active_channel.send(f'{input}')
             print(f'{timef} [CONSOLE] -> Sent `{input}` to #{str(client.active_channel)}\n')
         else:
-            print(f'{timef} [CONSOLE] -> Active guild has not been set this session run `[]set_guild` and `[]set_channel` in a channel or set them both with `[]set`.\n')
+            print(f'{timef} [CONSOLE] -> Active guild has not been set this session run `[]set` in a channel.\n')
 
 @client.command(name='autocatcher', aliases=['ac'])
 async def autocatcher_(ctx):
